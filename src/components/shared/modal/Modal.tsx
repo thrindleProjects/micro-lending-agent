4ef3db819fc11887cfaea1b6ad6fed92fee7bec5
mightyFZeus <bolarinolabisi36@gmail.com>
2023-02-08 08:49:58 +0100

    fix: changes

--- src/components/shared/modal/Modal.tsx
+++ src/components/shared/modal/Modal.tsx
@@ -9,7 +9,7 @@ const Modal: React.FC<MobileProps> = ({
   return (
     <MainContentLayout>
       <div
-        style={{ backgroundColor: 'rgba(0,0,0, 0.5)' }}
+        style={{ backgroundColor: '#00000020' }}
         className='fixed inset-0 z-[99999] overflow-y-scroll md:flex md:items-center md:justify-center'
       >
         <div
