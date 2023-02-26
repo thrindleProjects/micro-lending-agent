f53265b2a1d251661130df37766ce04a6c93ca9e
khallekan <okeolalekanisaac@gmail.com>
2023-02-26 21:08:12 +0100

    style: update style of login form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
-import { GetServerSideProps, NextPage } from 'next';
-
+import { GetStaticProps, NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 const HomePage: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/home');
+  }, [router]);
+
   return <></>;
 };
 
 export default HomePage;
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   return {
     redirect: {
+      source: '/',
       destination: '/home',
       permanent: true,
     },
