923181f15e975f7ce6d47693ce1a58bd1033728a
khallekan <okeolalekanisaac@gmail.com>
2023-03-07 15:49:08 +0100

    refactor: middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,30 @@
-import { withAuth } from 'next-auth/middleware';
+export { default } from 'next-auth/middleware';
 
-export default withAuth(
-  function middleware(req) {
-    const session = req.nextauth.token?.user;
+// export default withAuth(
+//   function middleware(req) {
+//     const session = req.nextauth.token?.user;
 
-    if (
-      session &&
-      !(
-        session.completedBank &&
-        session.completedBusiness &&
-        session.completedContact &&
-        session.completedUploads
-      )
-    ) {
-      // return NextResponse.redirect(new URL('/complete-registration', req.url));
-    }
-  },
-  {
-    callbacks: {
-      authorized: (session) => !!session.token,
-    },
-  }
-);
+//     if (
+//       session &&
+//       !(
+//         session.completedBank &&
+//         session.completedBusiness &&
+//         session.completedContact &&
+//         session.completedUploads
+//       )
+//     ) {
+//       // return NextResponse.redirect(new URL('/complete-registration', req.url));
+//     }
+//   },
+//   {
+//     callbacks: {
+//       authorized: (session) => !!session.token,
+//     },
+//   }
+// );
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|login|register|assets|serviceWorker|fonts|complete-registration).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|login|register|assets|serviceWorker|fonts).*)',
   ],
 };
